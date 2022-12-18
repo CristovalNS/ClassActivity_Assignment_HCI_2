d66ac642a057ff9d1be9d0a60b378bde7ba7d789
Cristoval Neo Sasono <cristovalsasono@outlook.com>
2022-12-18 19:23:43 +0700

    Added implementation.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // Add your answer here
+
+$('.lesson').hover(
+    function() {
+        $(this).find('.text-contents').css('display', 'block');
+    },
+    function() {
+        $(this).find('.text-contents').css('display', 'none');
+    }
+);
+
